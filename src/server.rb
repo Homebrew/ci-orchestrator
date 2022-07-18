a6e117e904dec4955a4465ae8fac533fda7d148d
Bo Anderson <mail@boanderson.me>
2022-07-18 16:08:26 +0100

    Update dependencies

--- src/server.rb
+++ src/server.rb
@@ -12,6 +12,7 @@ class CIOrchestratorApp < Sinatra::Base
   configure do
     set :sessions, expire_after: 28800, same_site: :lax, skip: true
     set :session_store, Rack::Session::Pool
+    set :protection, reaction: :warn
   end
 
   set(:require_auth) do |enabled|
