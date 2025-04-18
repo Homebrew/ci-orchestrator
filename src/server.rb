095adfae8e9d90704238ffa5aad7e2f4930d8bf8
Bo Anderson <mail@boanderson.me>
2025-04-18 01:47:13 +0100

    server: handle runner ready hook better with Orka 3

--- src/server.rb
+++ src/server.rb
@@ -249,9 +249,7 @@ class CIOrchestratorApp < Sinatra::Base
 
     vm_id = params["orka_vm_id"]
 
-    if job.orka_vm_id.nil?
-      return if job.github_state == :completed && job.orka_setup_complete?
-
+    if job.orka_vm_id.nil? || (job.orka_vm_id == vm_id && !job.orka_setup_complete?)
       job.orka_vm_id = vm_id
       job.orka_setup_time = Time.now.to_i
       job.orka_setup_timeout = false
